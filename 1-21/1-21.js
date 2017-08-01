9e03b6424fef18eda6b6b3de7f9720d62b317e51
Cor <805751525@qq.com>
2017-08-01 17:35:38 +0800

    complete 1-21

--- 1-21/1-21.js
+++ 1-21/1-21.js
@@ -1,127 +1,214 @@
-function addEvent (ele, type, handler) {
-	if(ele.addEventListener){
-		ele.addEventListener(type, handler, false);
-	}else if(ele.attachEvent){
-		ele.attachEvent("on" + type, function(){
-			handler.apply(ele);
-		});
-	}else{
-		ele[on + "type"] = handler;
-	}
-}
+// function addEvent (ele, type, handler) {
+// 	if(ele.addEventListener){
+// 		ele.addEventListener(type, handler, false);
+// 	}else if(ele.attachEvent){
+// 		ele.attachEvent("on" + type, function(){
+// 			handler.apply(ele);
+// 		});
+// 	}else{
+// 		ele[on + "type"] = handler;
+// 	}
+// }
 
-function splitInput (text) {
-	var inputArr = [];
-	inputArr = text.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).filter(function (item) {
-		return item.length !== 0;
-	});
-	return inputArr;
-}
+// function splitInput (text) {
+// 	var inputArr = [];
+// 	inputArr = text.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/).filter(function (item) {
+// 		return item.length !== 0;
+// 	});
+// 	return inputArr;
+// }
 
-	var container = document.getElementById("container");
-	var buttonList = document.getElementsByTagName("input");
-	var oText = document.getElementById("inputBox");
+// 	var textInput = document.getElementById("textInput");
+// 	var hobbyInput = document.getElementById("hobbyInput");
 
-	var queue = {
-		str : [],
+// 	var textBox = document.getElementById("container");
+// 	var hobbyBox = document.getElementsByTagName("input");
 
-		leftPush : function (arr) {
-			for(var i = arr.length - 1; i >= 0; i--){
-				this.str.unshift(arr[i]);
-			}
-			this.paint();
-		},
+// 	var hobbyBtn = document.getElementById("hobbyBtn");
 
-		rightPush : function (arr) {
-			for(var i = 0; i < arr.length; i++){
-				this.str.push(arr[i]);
-			}
-			this.paint();
-		},
+// 	var queue = {
+// 		str : [],
 
-		isEmpty : function () {
-			return (this.str.length == 0);
-		},
+// 		push : function (arr) {
+// 			for(var i = 0; i < arr.length; i++){
+// 				this.str.push(arr[i]);
+// 			}
+// 			this.paint();
+// 		},
 
-		leftPop : function () {
-			if(!this.isEmpty()){
-				console.log(this.str.shift());
-				this.paint();
-			}else{
-				console.log("The queue is already empty!");
-			}
-		},
+// 		rightPop : function () {
+// 			if(!this.isEmpty()){
+// 				console.log(this.str.pop());
+// 				this.paint();
+// 			}else{
+// 				console.log("The queue is already empty!");
+// 			} 
+// 		},
 
-		rightPop : function () {
-			if(!this.isEmpty()){
-				console.log(this.str.pop());
-				this.paint();
-			}else{
-				console.log("The queue is already empty!");
-			} 
-		},
+// 		paint : function(str){
+// 			var strInner = "";
 
-		paint : function(str){
-			var strInner = "";
+// 			if(str){
+// 				strInner = this.str.map(function(e){
+// 					var repText = e.replace(new RegExp(str, "g"), "<span class='z-sch'>" + str + "</span>");
+// 					return "<div>" + repText + "</div>";
+// 				}).join("");
 
-			if(str){
-				strInner = this.str.map(function(e){
-					var repText = e.replace(new RegExp(str, "g"), "<span class='z-sch'>" + str + "</span>");
-					return "<div>" + repText + "</div>";
-				}).join("");
+// 				container.innerHTML = strInner;
+// 				return ;
+// 			}
 
-				container.innerHTML = strInner;
-				return ;
-			}
+// 			this.str.forEach(function(item, index){
+// 				strInner += "<div>" + item + "</div>";
+// 			});
 
-			this.str.forEach(function(item, index){
-				strInner += "<div>" + item + "</div>";
-			});
+// 			container.innerHTML = strInner;
 
-			container.innerHTML = strInner;
+// 			addDivDelEvent();
+// 		},
+ 
+// 		deleteID : function (id) {
+// 			console.log(this.str[id]);
+// 			this.str.splice(id, 1);
+// 			this.paint();
+// 		}
+// 	}
 
-			addDivDelEvent();
-		},
+// 	function addDivDelEvent () {
+// 		for(var cur = 0; cur < container.children.length; cur++){
+// 			addEvent(container.children[cur], "click", function(cur){
+// 				return function(){return queue.deleteID(cur)};
+// 			}(cur));
+// 		}
+// 	}
 
-		deleteID : function (id) {
-			console.log(this.str[id]);
-			this.str.splice(id, 1);
-			this.paint();
-		}
-	}
-
-	function addDivDelEvent () {
-		for(var cur = 0; cur < container.children.length; cur++){
-			addEvent(container.children[cur], "click", function(cur){
-				return function(){return queue.deleteID(cur)};
-			}(cur));
-		}
-	}
-
-	function searchDivContent (text) {
-		if(text.trim() == "" || container.children.length <= 1){
-			return false;
-		}
+// 	function searchDivContent (text) {
+// 		if(text.trim() == "" || container.children.length <= 1){
+// 			return false;
+// 		}
 		
-		queue.paint(text);
+// 		queue.paint(text);
+// 	}
+
+// 	// addEvent(textInput, "");
+
+// 	addEvent(hobbyBtn, "click", function(){
+// 		console.log("i am hobbyBtn");
+// 	});
+
+
+;(function IIFE(){
+	let Regular = /[^0-9a-zA-Z\u4e00-\u9fa5]+/,
+		Regular2 = /[&\|\\\*^%'".+_=,;}{?><~`$#@\s/-]/ig,
+		Regular3 = /[,\s\n]+/
+
+
+	function Queue (input, output, button){
+		this.input = document.querySelector(`#${input}`)
+		this.output = document.querySelector(`#${output}`)
+		this.button = document.querySelector(`#${button}`)
+		this.arr = []
+
+		this.button ? this.init('buttonEvent') : this.init('keyEvent')
 	}
 
-	addEvent(buttonList[0], "click", function(){
-		var input = splitInput(oText.value.trim());
-		queue.leftPush(input);
-	});
+	Queue.prototype = {
+		constructor: Queue,
 
-	addEvent(buttonList[1], "click", function () {
-		var input = splitInput(oText.value.trim());
-		queue.rightPush(input);
-	});
+		init: function(type){
+			this.setQueue(type)
+		},
 
-	addEvent(buttonList[2], "click", queue.leftPop.bind(queue));
+		setArr: function(num){
+			this.arr.splice(0, num)
+			this.render()
+		},
 
-	addEvent(buttonList[3], "click", queue.rightPop.bind(queue));
+		render: function(){
+			let num = 0
+			this.output.innerHTML = ''
+			for(let i = 0, l = this.arr.length; i < l; i++){
+				if(l > 10){
+					this.setArr(l - 10)
+					return
+				}else{
+					this.output.innerHTML += `<span index="${i}">${this.arr[i]}</span>`
+				}
+			}
+		},
 
-	addEvent(buttonList[5], "click", function(){
-		var inputValue = buttonList[4].value;
-		searchDivContent(inputValue);
-		addDivDelEvent();
-	});
+		setText: function(e){
+			if(e.target && e.target.nodeName === 'SPAN'){
+				switch(e.type){
+					case 'mouseover':
+						e.target.textContent = `删除：${e.target.textContent}`
+						break
+					case 'mouseout':
+						e.target.textContent = e.target.innerHTML.replace('删除：', '')
+						break
+					case 'click':
+						this.output.removeChild(e.target)
+						let index = e.target.getAttribute('index')
+						this.arr.splice(index, 1)
+						this.render()
+						break
+				}
+			}
+		},
+
+		addTag: function(e){
+			if(Regular3.test(e.target.value) || e.keyCode == 13){
+				let str = e.target.value.replace(Regular2, '')
+				this.arr.push(str)
+				this.arr = this.arr.unique()
+				e.target.value = ''
+				this.render()
+			}
+		},
+
+		addHobby: function(){
+			let str = this.input.value.trim()
+			this.input.value = ''
+			let data = str.split(Regular).filter((e) => e.length !== 0)
+			this.arr = this.arr.concat(data).unique()
+			this.render();
+		},
+
+		setQueue: function(type){
+			addEvent(this.output, 'mouseover', this.setText)
+			addEvent(this.output, 'mouseout', this.setText)
+			addEvent(this.output, 'click', this.setText.bind(this))
+
+			if(type === 'buttonEvent'){
+				addEvent(this.button, 'click', this.addHobby.bind(this))
+			}else{
+				addEvent(this.input, 'keyup', this.addTag.bind(this))
+			}
+		}
+	}
+
+	if(!Array.prototype.unique){
+		Array.prototype.unique = function(){
+			let res = []
+			this.forEach((item, i) => res.indexOf(item) === -1 ? res.push(item) : '')
+			return res
+		}
+	}
+
+	function addEvent (ele, type, handler) {
+		if(ele.addEventListener){
+			ele.addEventListener(type, handler, false)
+		}else if(ele.attachEvent){
+			ele.attachEvent("on" + type, function(){
+				handler.apply(ele)
+			})
+		}else{
+			ele[on + "type"] = handler
+		}
+	}
+
+	window.Queue = Queue
+})()
+
+new Queue('tagInput', 'tagBox')
+new Queue('hobbyInput', 'hobbyBox', 'hobbyBtn')
