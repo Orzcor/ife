440b576e087ceb79ed9b45fe8fa88f91201b6bf7
Cor <805751525@qq.com>
2017-08-22 20:41:29 +0800

    [upload] develop files: /src/

---
+++ 26/src/js/index.js
@@ -0,0 +1,12 @@
+import '../style/reset.css'
+import '../style/index.scss'
+
+/**
+ * @param {string} somebody - Somebody's name
+ */
+
+function sayhello (somebody) {
+    console.log('Hello ' + somebody)
+}
+
+sayhello('Jason')
